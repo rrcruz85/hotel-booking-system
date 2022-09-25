55cfd3dcebf63772a8c73be9bd016a696f277d06
reynaldo <reynaldo.rodriguez@applieddatacorp.com>
2022-09-24 20:16:31 -0500

    Removing spaces

--- HotelManagement/Hotel.Management.WebApi/Program.cs
+++ HotelManagement/Hotel.Management.WebApi/Program.cs
@@ -20,8 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseUnhandledExceptionHandler();
